ab784ca4a96009b1e9e90b9d2ff1ba9b3a0ce65e
erik-olivier riendeau <erikolivierdr@gmail.com>
2020-09-22 22:16:29 -0400

    various fixes

--- test_project/benchmark_utilities.py
+++ test_project/benchmark_utilities.py
@@ -1,16 +1,15 @@
 import re
 from utilities import StringConverters
-
+import timeit
 
 if __name__ == '__main__':
-    import timeit
+    sc = StringConverters()
+    with open("sherlock.txt") as f:
+        lines = f.readlines()
     s = """
-sc = StringConverters()
-with open("sherlock.txt") as f:
-    lines = f.readlines()
 meme_lines = [sc.translate_to_internet_meme(l) for l in lines[:100]]
     """
 
-    print(timeit.timeit(stmt=s, setup="from __main__ import  StringConverters", number=1000))
+    print(timeit.timeit(stmt=s, setup="from __main__ import  sc, lines", number=1000))
 
 
